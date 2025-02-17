c91a79d1783c12074fe3c307bdacba5fb1573199
SubhaManshaDeviS <146518953+SubhaManshaDeviS@users.noreply.github.com>
2025-02-17 09:47:46 +0530

    Create factors.js

---
+++ Class hands-on/Set/Java Script- 2/factors.js
@@ -0,0 +1,11 @@
+function Factor(num) {
+    console.log(`Factors of ${num}:`);
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+        }
+    }
+}
+
+
+Factor(24);
