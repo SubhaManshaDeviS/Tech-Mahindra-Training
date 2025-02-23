7126d525cd7627bfcdc83f31b06a2b38edcbacd6
SubhaManshaDeviS <146518953+SubhaManshaDeviS@users.noreply.github.com>
2025-02-24 00:39:36 +0530

    Create Task_3.java

---
+++ Week-3/Task 1/Task_3.java
@@ -0,0 +1,19 @@
+import java.util.Objects;
+
+public class EqualsVsDeepEquals { 
+  public static void main(String[] args) { 
+    Integer[] arr1 = {1, 2, 3}; 
+    Integer[] arr2 = {1, 2, 3}; 
+    Integer[] arr3 = arr1;
+
+System.out.println("Using equals():");
+    System.out.println("arr1.equals(arr2): " + arr1.equals(arr2)); 
+    System.out.println("arr1.equals(arr3): " + arr1.equals(arr3)); 
+    
+    System.out.println("\nUsing deepEquals():");
+    System.out.println("Objects.deepEquals(arr1, arr2): " + Objects.deepEquals(arr1, arr2));
+    System.out.println("Objects.deepEquals(arr1, arr3): " + Objects.deepEquals(arr1, arr3)); 
+}
+
+}
+
