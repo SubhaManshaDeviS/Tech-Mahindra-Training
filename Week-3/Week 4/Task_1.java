2d1038075e9006bae1bba54eba907a13a13690ac
SubhaManshaDeviS <146518953+SubhaManshaDeviS@users.noreply.github.com>
2025-02-24 07:54:28 +0530

    Delete Week-3/Week 4 directory

--- Week-3/Week 4/Task_1.java
+++
@@ -1,9 +0,0 @@
-public class ExceptionHandling {
-    public static void main(String[] args) {
-        try {
-            int res = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("Exception caught: " + e.getMessage());
-        }
-    }
-}
