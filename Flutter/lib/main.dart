48835cca625168b24e3adf6972600133299e66d4
SubhaManshaDeviS <146518953+SubhaManshaDeviS@users.noreply.github.com>
2025-04-01 17:33:03 +0530

    Add files via upload

---
+++ Flutter/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/home_page.dart';
+import 'screens/playlist_page.dart';
+import 'screens/songs_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Music Manager',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color.fromARGB(255, 166, 11, 55),
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    PlaylistPage(),
+    SongsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: Colors.blueGrey[900],
+        selectedItemColor: const Color.fromARGB(255, 178, 9, 82),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.playlist_play), label: 'Playlists'),
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Songs'),
+        ],
+      ),
+    );
+  }
+}
